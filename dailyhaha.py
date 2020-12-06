51820b3536a29a68a420cc3abf590172047cca4b
yug damor <yugdamor130296@gmail.com>
2020-12-07 02:22:15 +0530

    Add files via upload

--- dailyhaha.py
+++ dailyhaha.py
@@ -1,6 +1,5 @@
 
 import pip
-
 def install(package):
     if hasattr(pip, 'main'):
         pip.main(['install', package])
@@ -9,6 +8,8 @@ def install(package):
 install("bs4")
 install("kora")
 install("selenium")
+install("tqdm")
+from tqdm import tqdm
 from datetime import datetime
 from kora.selenium import wd
 from bs4 import BeautifulSoup
@@ -30,33 +31,41 @@ all_data = []
 
 # wd = webdriver.Chrome(ChromeDriverManager().install())
 # 1293
-for i in range(1292,1294):
-  wd.get(url2.format(i))
-  time.sleep(1)
-  soup = BeautifulSoup(wd.page_source)
-  all_links = []
-  for i in soup.find_all("a",{"class","item gif"}):
-    all_links.append(i.get("href"))
-  for i in soup.find_all("a",{"class","item picture"}):
-    all_links.append(i.get("href"))
-  for link in all_links:
-    wd.get(link)
-    time.sleep(1)
-    item_soup = BeautifulSoup(wd.page_source)
-    category = item_soup.find("div",{"class":"category"}).a.text
-    type_content = ""
-    print(item_soup.find("div",{"class":"single"}).figure.get("class"))
-    type_of_content = item_soup.find("div",{"class":"single"}).figure.get("class")[0]
-    link_content = ""
-    title = item_soup.find("div",{"class":"h1d"}).h1.text
-    if type_of_content == "picture-view":
-      type_content = "IMAGE"
-      link_content = "https://www.dailyhaha.com{}".format(item_soup.find("div",{"class":"picholder"}).img.get("src"))
-    if type_of_content == "gif-view":
-      type_content = "VIDEO"
-      link_content = "https://www.dailyhaha.com{}".format(item_soup.find("div",{"class":"gifholder"}).find("source").get("src"))
-    all_data.append({"ContentTitle":title,"Description":"","ContentType":type_content,"ContentLink":link_content,"Tags":[category]})
+print("scrapping all links : started")
+all_links = []
+i =0
+wd.get("https://www.dailyhaha.com/archive/1")
+while(wd.title != "404"):
+    try:
+        i = i + 1
+        wd.get(url2.format(i))
+        soup = BeautifulSoup(wd.page_source,features="lxml")
+        for i in soup.find_all("a",{"class","item gif"}):
+            all_links.append(i.get("href"))
+        for i in soup.find_all("a",{"class","item picture"}):
+            all_links.append(i.get("href"))
+    except:
+        pass
+print("scrapping all links : done")
 
+for link in tqdm(all_links):
+    try:
+        wd.get(link)
+        item_soup = BeautifulSoup(wd.page_source,features="lxml")
+        category = item_soup.find("div",{"class":"category"}).a.text
+        type_content = ""
+        type_of_content = item_soup.find("div",{"class":"single"}).figure.get("class")[0]
+        link_content = ""
+        title = item_soup.find("div",{"class":"h1d"}).h1.text
+        if type_of_content == "picture-view":
+          type_content = "IMAGE"
+          link_content = "https://www.dailyhaha.com{}".format(item_soup.find("div",{"class":"picholder"}).img.get("src"))
+        if type_of_content == "gif-view":
+          type_content = "VIDEO"
+          link_content = "https://www.dailyhaha.com{}".format(item_soup.find("div",{"class":"gifholder"}).find("source").get("src"))
+        all_data.append({"ContentTitle":title,"Description":"","ContentType":type_content,"ContentLink":link_content,"Tags":[category]})
+    except:
+        pass
 now = datetime.now().strftime('%d-%m-%Y')
 
 try:
